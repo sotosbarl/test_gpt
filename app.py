8eae2d70d760293507eabbf4c1842765a92b65bf
Sotirios Barlakas <57687239+sotosbarl@users.noreply.github.com>
2024-01-02 16:54:14 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from transformers import pipeline,AutoModelForSeq2SeqLM
+import streamlit as st
+
+model_id = "lmsys/fastchat-t5-3b-v1.0"  # Replace with your model's name on the hub
+
+# pipe = pipeline("text2text-generation", model="lmsys/fastchat-t5-3b-v1.0")
+
+# model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+pipe = pipeline("text2text-generation")
+
+text = st.text_area('enter some text:')
+if text:
+    out = pipe(text)
+
+    st.json(out)
