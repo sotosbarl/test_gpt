5053b0d80b83af23e19bb05923fdcff836c422fb
Sotirios Barlakas <57687239+sotosbarl@users.noreply.github.com>
2024-01-02 17:02:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ model_id = "lmsys/fastchat-t5-3b-v1.0"  # Replace with your model's name on the 
 
 # pipe = pipeline("text2text-generation", model="lmsys/fastchat-t5-3b-v1.0")
 
-# model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
-pipe = pipeline("text2text-generation")
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+pipe = pipeline("text2text-generation",model=model)
 
 text = st.text_area('enter some text:')
 if text:
